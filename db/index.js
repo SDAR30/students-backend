3ddbda2dc34c75c2b927caa4e96a046ed85e848f
Shoaib Dar <SDAR30@gmail.com>
2022-11-20 17:22:30 -0500

    db setup part 6

--- db/index.js
+++ db/index.js
@@ -32,7 +32,7 @@ require("dotenv").config();
 const { DATABASE_URL, PG_HOST, PG_PORT, PG_DATABASE, PG_USER  } = process.env;
 const cn = DATABASE_URL
   ? {
-      connectionString: DB_URL,
+      connectionString: DATABASE_URL,
       max: 30,
       ssl: {
         rejectUnauthorized: false,

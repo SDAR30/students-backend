aba73850fb0ae3990e7c819e2074de90fea6c81e
Shoaib Dar <SDAR30@gmail.com>
2022-11-20 17:09:16 -0500

    db setup part 4

--- db/index.js
+++ db/index.js
@@ -30,7 +30,7 @@ const pgp = require('pg-promise')();
 require("dotenv").config();
 
 const { DB_URL, PG_HOST, PG_PORT, PG_DATABASE, PG_USER  } = process.env;
-const cn = DB_URL
+const cn = DATABASE_URL
   ? {
       connectionString: DB_URL,
       max: 30,
---
+++ javascript/practice4.js
@@ -0,0 +1,5 @@
+let arr = ['a','b','b','c','c','b','g','h','f','g'];
+
+//console.log('miamidolphins'.split('').filter(letter => ['a','e','i','o','u'].indexOf(letter)!==-1).length)
+
+console.log('NEWyorkjetsJETS'.match(/[aeiou]/gi))
